fa6f4c114256ab04bc06008a5fa693e69932c848
Sophie BRULTET <sophie.brultet@gmail.com>
2020-12-07 12:09:46 +0100

    Quete Templating Razor

---
+++ Controllers/ExpenditureController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Quete_RazorTemplating.Models;
+
+namespace Quete_RazorTemplating.Controllers
+{
+    public class ExpenditureController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Expenditure> expend = new List<Expenditure> { new Expenditure { Name="Fournitures", Date="05/12/2020", Amount=250 },
+                                                               new Expenditure { Name="Bois", Date="08/12/2020", Amount=400 },
+                                                               new Expenditure { Name="Cadeaux", Date="05/12/2020", Amount=300 }};
+            return View(expend);
+        }
+    }
+}
---
+++ Models/Expenditure.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Quete_RazorTemplating.Models
+{
+    public class Expenditure
+    {
+        public Int32 ExpenditureId { get; set; }
+
+        public String Name { get; set; }
+
+        public String Date { get; set; }
+
+        public Double Amount { get; set; }
+    }
+}
